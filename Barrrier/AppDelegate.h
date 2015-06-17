0fac3b03bdb28a7c9a725cfaa4094e5795fdfb42
chenchaocc <ccerdong@sina.cn>
2015-06-17 21:26:43 +0800

    Initial Commit

---
+++ Barrrier/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Barrrier
+//
+//  Created by mac on 15/6/17.
+//  Copyright (c) 2015年 CC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Barrrier/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Barrrier
+//
+//  Created by mac on 15/6/17.
+//  Copyright (c) 2015年 CC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
